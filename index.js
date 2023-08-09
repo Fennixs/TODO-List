415d0418314bfe2333fe7dbcb4d394c43adf3bbe
Fennixs <barutf1500@gmail.com>
2023-08-09 16:40:32 -0500

    updated port

--- index.js
+++ index.js
@@ -1,62 +1,59 @@
 const express = require("express");
-const db = require('./db');
+const db = require("./db");
 const app = express();
-const port = 3000;
+const port = 8000;
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(express.static("public"));
 
-const list = [];
-
 // Route to fetch the list of items from the database and return them as JSON.
 app.get("/api/list", (req, res) => {
-  db.query('SELECT * FROM todolist')
-  .then((dbRes) => {
-    res.json(dbRes.rows);
-  })
-  .catch(console.error);
+  db.query("SELECT * FROM todolist")
+    .then((dbRes) => {
+      res.json(dbRes.rows);
+    })
+    .catch(console.error);
 });
 
 // Inserts item route
 app.post("/api/list", async (req, res) => {
   const body = req.body;
   const sql = `INSERT INTO todolist (text)
-  VALUES ($1) RETURNING *;`
+  VALUES ($1) RETURNING *;`;
   const dbRes = await db.query(sql, [body.text]);
   res.json(dbRes.rows);
 });
 
 // PUT route to update the completion status of a list item
-app.put("/api/list", async (req, res) => { 
-  const {completed, id} = req.body; 
+app.put("/api/list", async (req, res) => {
+  const { completed, id } = req.body;
 
-  db.query('UPDATE todolist SET completed = $1 WHERE id = $2', [completed, id])
+  db.query("UPDATE todolist SET completed = $1 WHERE id = $2", [completed, id])
     .then((dbRes) => {
-      console.log('Item successfully updated in the database.');
-      res.status(200).json(dbRes.rows); 
+      console.log("Item successfully updated in the database.");
+      res.status(200).json(dbRes.rows);
     })
     .catch((err) => {
-      console.error('Error updating item:', err);
+      console.error("Error updating item:", err);
       res.sendStatus(500);
     });
 });
 
 // Delete item route with "/:itemId" parameter
 app.delete("/api/list", async (req, res) => {
-  const {id} = req.body;
+  const { id } = req.body;
 
-  db.query('DELETE FROM todolist WHERE id = $1', [id])
-  .then((dbRes) =>{ 
-    res.sendStatus(200);
-  })
+  db.query("DELETE FROM todolist WHERE id = $1", [id])
+    .then((dbRes) => {
+      res.sendStatus(200);
+    })
     .catch((err) => {
-      console.error('Error deleting item:', err);
+      console.error("Error deleting item:", err);
     });
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
- 
