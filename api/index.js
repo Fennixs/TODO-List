5dc71b0683e919506e9152b8dc1c80f30477aa0f
Fennixs <barutf1500@gmail.com>
2023-08-09 16:51:34 -0500

    formatting app for vercel

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const db = require("./db");
+const db = require("../db");
 const app = express();
-const port = 8000;
+const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
