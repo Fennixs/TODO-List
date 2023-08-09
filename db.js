c4018e527d20b9dcb80b4c153525a7e31cc22f71
Fennixs <barutf1500@gmail.com>
2023-08-09 16:34:28 -0500

    fixed bug

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 // get the client
-const {Client} = require('pg');
+const { Client } = require("pg");
 require("dotenv").config();
 const client = new Client({
   host: process.env.POSTGRES_HOST,
@@ -7,17 +7,11 @@ const client = new Client({
   database: process.env.POSTGRES_DATABASE,
   password: process.env.POSTGRES_PASSWORD,
   port: 5432,
-})
+  ssl: true,
+});
 
-client.connect(); 
+client.connect();
 
 module.exports = client;
 
-
 //client.query('SELECT * FROM todolist').then((res) => console.log(res.rows))
-
-
- 
-
-
-
